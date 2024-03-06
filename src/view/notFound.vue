737943a447f6e36c8ad5e335cee13a12906e00ff
Hanaly_ <miantrajulianah@gmail.com>
2024-03-06 20:14:58 +0100

    kisdkisdiksd

--- src/view/notFound.vue
+++ src/view/notFound.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="hello">
-    <h1 class=" bg-red-500">{{ msg }}</h1>
-    <span class=" text-red-500">je taime</span>
+  <div class=" "> 
+    ce page n'existe pas
    
   </div>
 </template>
