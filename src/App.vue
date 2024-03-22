e6e007bab3a984cd400e45d2d04d4433e1d1abec
Dama Dany <damadanyna@hgmail.com>
2024-03-22 19:46:31 +0300

    About designed Changes to be committed: 	modified:   src/App.vue 	new file:   src/view/aboutView.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
 <div class=" overflow-auto overflow-x-hidden w-full h-full">
     <accueilView></accueilView>
     <horizontalBarVue></horizontalBarVue>
-    <accueilView></accueilView>
-
+    <aboutVue></aboutVue>
 </div>
 </template>
 
 <script>
 import horizontalBarVue from './components/horizontalBar.vue'
+import aboutVue from './view/aboutView.vue';
 import accueilView from './view/accueilView.vue';
 export default {
     name: 'App',
@@ -19,7 +19,8 @@ export default {
 
     components: {
         accueilView,
-        horizontalBarVue
+        horizontalBarVue,
+        aboutVue
     },
     mounted() {
 
---
+++ src/view/aboutView.vue
@@ -0,0 +1,30 @@
+<template>
+<div class=" h-[110vh] w-full flex mt-20  ">
+    <div class=" flex w-[60vw] justify-end">
+        <img class=" w-[40rem] h-[38rem]" src="../assets/img/about-img.png" alt="">
+    </div>
+    <div class=" flex w-[45vw]  ml-8 flex-col justify-center mb-28 ">
+        <div class=" text-3xl  font-semibold flex items-center">
+            <div class="relative">
+                <div class="absolute mt-4 h-[3px] w-[57px] bg-[#4edab2]"></div>
+            </div>
+            <span>About Design</span>
+            <div class=" w-[.67rem] h-[.67rem] bg-[#fb1004] rounded-md ml-2"></div>
+        </div>
+        <div class=" w-[34vw] mt-10">
+            <span>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Contenthere , content here', making it</span>
+        </div>
+        <button class=" hover:bg-red-500 mt-9 w-[11rem] rounded-md bg-[#fb1004] text-white py-3">About More</button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
