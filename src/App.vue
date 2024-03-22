174cf6c96bddfaa512e3e70d62ca823244c857a4
Dama Dany <damadanyna@hgmail.com>
2024-03-22 15:12:01 +0300

    initialisatoin dru portFolio

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,27 @@
 <template>
-<div class=" flex w-full h-full duration-1000" :class="this.$store.state.theme[this.$store.state.indeOfTheme].bg">
- 
-    <notify_ id="note_" class=""></notify_>
-    <popRecherche_ v-if="this.$store.state.hidePopup==false"> </popRecherche_>
-    <popupDec v-if="this.$store.state.hidePopupUser==false"></popupDec>
-    <div v-if="this.$store.state.logged == true" class=" w-full h-full">
-        <div></div>
-        <horizontal_bar :class="this.$store.state.theme[this.$store.state.indeOfTheme].bg_h_nav"></horizontal_bar>
-        <div class=" flex flex-row h-full ">
-            <navBar></navBar>
-            <router-view></router-view>
-        </div>
-    </div>  
-
-    <login_Vue class="w-full " v-else></login_Vue>
+<div class=" flex w-full h-full"> 
+    <accueilView></accueilView>
 </div>
-<!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
-<script> 
-import popRecherche_ from './components/popRecherche_.vue'
-import popupDec from './components/poputDecnx.vue'
-import horizontal_bar from './components/horizontalBar.vue'
-import navBar from './components/navBar.vue'
-import login_Vue from './view/auth/login_.vue'
-import notify_ from '@/components/notify_'
+<script>
+ import accueilView from './view/accueilView.vue';
+  export default {
+      name: 'App',
 
-export default {
-    name: 'App',
+      data() {
+          return {}
+      },
 
-    data() {
-        return {}
-    },
+      components: {
+        accueilView
+      },
+      mounted() {
 
-    components: {
-        popRecherche_,
-        horizontal_bar,
-        navBar,
-        login_Vue,
-        notify_,popupDec
-    },
-    mounted() {
-        document.getElementById('c_load').style.display = 'none'
-        // console.log(this.$store.state.theme[this.$store.state.indeOfTheme]);
+      },
+      updated() {}
 
-        // var actRout= document.querySelector('.router-link-exact-active ') 
-        // actRout.style.color="#00c93c";
-        //     color: #00c93c;  
-        // border-left: 2px solid #00c93c;
-        // background-color: rgb(237, 253, 237);
-        // animation: animeit 500 forwards;
-    },
-    beforeMount() {
-
-        var data = JSON.parse(window.localStorage.getItem('*_*')) ? JSON.parse(window.localStorage.getItem('*_*')) : '';
-        this.$store.state.logged = data.isLg;
-    },
-    updated() {
-        // this.theme= this.$store.state.theme
-        //     this.i_= this.$store.state.indeOfTheme 
-    }
-
-}
+  }
 </script>
 
 <style>
---
+++ src/view/accueilView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class=" w-full h-full object-cover bg-center " style="background-image: url('../assets/img/about-img.png');"> 
+        accueilView
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
